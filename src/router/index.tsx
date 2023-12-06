3455ba6db396e12315521da3a3d4f12955ad423c
Adrián Santos Mena <adrian.santos.mena.contractor@bbva.com>
2023-12-06 19:47:38 +0100

    Testing nested routes adding basename to createBrowserRouter function.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -51,74 +51,77 @@ const Navigate: React.FC = () => {
     );
 };
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MainApp id="main-app" />,
-        children: [
-            {
-                path: "/",
-                element: (
-                    <>
-                        <Navbar id="navbar-test"></Navbar>
-                        <Outlet></Outlet>
-                    </>
-                ),
-                children: [
-                    {
-                        path: "/",
-                        element: <Navigate />,
-                    },
-                    {
-                        path: "react-router-installation",
-                        element: (
-                            <BlogTemplate id="react-router-blog-template">
-                                <ReactRouterInstallation id="react-router-installation-page" />
-                            </BlogTemplate>
-                        ),
-                    },
-                    {
-                        path: "component-library",
-                        element: (
-                            <BlogTemplate id="component-library-template">
-                                <ComponentLibrary id="component-library-page" />
-                            </BlogTemplate>
-                        ),
-                    },
-                    {
-                        path: "app",
-                        element: <App />,
-                    },
-                    {
-                        path: "web-utilities",
-                        element: <WebUtilities id="web-utilities-page" />,
-                    },
-                    {
-                        path: "nested-1",
-                        element: (
-                            <div>
-                                First nested page!! <Outlet />
-                            </div>
-                        ),
-                        children: [
-                            {
-                                path: "nested-2",
-                                element: <div>Second nested page!!</div>,
-                            },
-                            {
-                                path: "test-component",
-                                element: <TestComponent />,
-                            },
-                        ],
-                    },
-                ],
-            },
-            {
-                path: "portfolio",
-                element: <Portfolio id="portfolio-page" />,
-            },
-        ],
-    },
-]);
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <MainApp id="main-app" />,
+            children: [
+                {
+                    path: "/",
+                    element: (
+                        <>
+                            <Navbar id="navbar-test"></Navbar>
+                            <Outlet></Outlet>
+                        </>
+                    ),
+                    children: [
+                        {
+                            path: "/",
+                            element: <Navigate />,
+                        },
+                        {
+                            path: "react-router-installation",
+                            element: (
+                                <BlogTemplate id="react-router-blog-template">
+                                    <ReactRouterInstallation id="react-router-installation-page" />
+                                </BlogTemplate>
+                            ),
+                        },
+                        {
+                            path: "component-library",
+                            element: (
+                                <BlogTemplate id="component-library-template">
+                                    <ComponentLibrary id="component-library-page" />
+                                </BlogTemplate>
+                            ),
+                        },
+                        {
+                            path: "app",
+                            element: <App />,
+                        },
+                        {
+                            path: "web-utilities",
+                            element: <WebUtilities id="web-utilities-page" />,
+                        },
+                        {
+                            path: "nested-1",
+                            element: (
+                                <div>
+                                    First nested page!! <Outlet />
+                                </div>
+                            ),
+                            children: [
+                                {
+                                    path: "nested-2",
+                                    element: <div>Second nested page!!</div>,
+                                },
+                                {
+                                    path: "test-component",
+                                    element: <TestComponent />,
+                                },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    path: "portfolio",
+                    element: <Portfolio id="portfolio-page" />,
+                },
+            ],
+        },
+    ],
+    { basename: "/" }
+);
 
 export default router;
